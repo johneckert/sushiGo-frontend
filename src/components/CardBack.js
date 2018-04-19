01d46691248ea6261b6686efbf8ae8e2f74f9897
johneckert <johnteckert@gmail.com>
2018-04-18 22:22:04 -0400

    finished css for back of card

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const CardBack = props => {
-  return <div>CardBack</div>;
+  return (
+    <div className="card back">
+      <ul>
+        {props.rollData.ingredients
+          .split(', ')
+          .map((ingredient, index) => <li key={index}>{ingredient}</li>)}
+      </ul>
+      <div className="line" />
+    </div>
+  );
 };
 
 export default CardBack;
